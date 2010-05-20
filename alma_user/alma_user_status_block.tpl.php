47ea32e5e390de6a9d1059ae0af084216a1e102a
Kasper Garnæs <kasper@reload.dk>
2010-05-20 16:10:18 +0200

    Moved default template files from Dynamo to modules. #1308

--- alma_user/alma_user_status_block.tpl.php
+++ alma_user/alma_user_status_block.tpl.php
@@ -5,32 +5,67 @@
  * @file alma_user_status_block.tpl.php
  * Template for the user status block.
  */
+ 
+/*
+ * TODO get status from mikl
+ */
+if( $user_status['loan_overdue_count'] >= 1){
+  $loan_status  = "warning";
+}
+else{
+  $loan_status  = "default";
+}
+  
+if( $user_status['reservation_count'] >= 1){
+  $reservation_status = "ok";
+}
+else{
+  $reservation_status = "default";
+  
+}
 ?>
-<div class="logout-link"><?php print l('Log out', 'logout'); ?></div>
+<div id="account-profile" class="clearfix">
+	<div class="user">
 
-<div class="welcome-username">
-  <span class="welcome"><?php print t('Welcome'); ?></span>
-  <span class="user-name"><?php print l($display_name, 'user/' . $user->uid . '/profile'); ?></span>
+		<div class="logout">
+			<?php print l(t('log out'), 'logout', array('attributes' => array('class' =>'logout'))); ?>
+		</div>
+
+		<h5><?php print t('Welcome'); ?></h5>
+		<div class="username">
+			<?php print l($display_name, $profile_link, array('attributes' => array('class' =>'username')));  ?>
+		</div>
+
+	</div>
+	<?php if ($user_status_available): ?>
+		<div class="cart">
+	    <div class="count"><?php print $cart_count; ?></div>
+	    <?php print l(t('Go to cart'), 'user/' . $user->uid . '/cart'); ?>
+		</div>
+
+		<ul>
+	    <li>
+
+	      <div class="content">
+					<?php print l('<span>'.t("Loans") . '</span> <strong>' . $user_status['loan_count'] . '</strong>', 'user/'. $user->uid . '/status', array('html' => TRUE)); ?>
+				</div>
+        <?php if($loan_status != "default"){ ?>
+				  <div class="status"><span class="<?php print $loan_status ?>">!</span></div>
+        <?php } ?>
+	    </li>
+	    <li>
+				<div class="content">
+	        <?php print l('<span>'.t("Reservations") . '</span> <strong>' . $user_status['reservation_count'] . '</strong>', 'user/'. $user->uid . '/status', array('html' => TRUE, 'fragment' => 'reservation')); ?>
+				</div>
+        <?php if($reservation_status  != "default"){ ?>				
+				  <div class="status"><span class="<?php print $reservation_status ?>">ok</span></div>
+        <?php } ?>
+
+	    </li>
+	</ul>
+	<?php else: ?>
+	  <div class="status-unavailable">
+	    <?php print $status_unavailable_message; ?>
+	  </div>
+	<?php endif; ?>
 </div>
-
-<div class="alma-patron-status <?php print $wrapper_classes; ?>">
-<?php if (!$user_status_unavailable): ?>
-  <div class="cart-status">
-    <span class="count"><?php print $cart_count; ?></span>
-    <?php print l('Go to cart', 'patron/cart'); ?>
-  </div>
-  <ul class="materials">
-    <li class="loans">
-      <?php print l(t("Loans") . ' <span class="count">' . $user_status['loan_count'] . '</span>', 'user/'. $user->uid . '/status', array('html' => TRUE)); ?>
-    </li>
-    <li class="reservations">
-      <?php print l(t("Reservations") . ' <span class="count">' . $user_status['reservation_count'] . '</span>', 'user/'. $user->uid . '/status', array('html' => TRUE, 'fragment' => 'reservation')); ?>
-    </li>
-  </ul>
-<?php else: ?>
-  <div class="status-unavailable">
-    <?php print $status_unavailable_message; ?>
-  </div>
-<?php endif; ?>
-</div>
-
